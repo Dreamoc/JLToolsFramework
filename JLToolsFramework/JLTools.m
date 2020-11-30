b952c637969a852555056f9db981470f35715af5
fengjinlin <461775260@qq.com>
2020-11-30 17:31:36 +0800

    添加项目

---
+++ JLToolsFramework/JLTools.m
@@ -0,0 +1,26 @@
+//
+//  JLTools.m
+//  JLToolsFramework
+//
+//  Created by Dreamoc on 2020/11/30.
+//
+
+#import "JLTools.h"
+
+@implementation JLTools
+- (NSString *)name {
+    return @"我的测试";
+}
+
+- (NSString *)email {
+    return @"我的email";
+}
+
+- (NSString *)tel {
+    return  @"我的tel";
+}
+
+- (NSString *)card {
+    return @"我的card";
+}
+@end
