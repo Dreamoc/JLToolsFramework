24ff4a9ce60d57d68826894fa90dfa33c055a87c
fengjinlin <461775260@qq.com>
2020-12-01 11:37:20 +0800

    a

---
+++ JLToolsFramework/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  JLToolsFramework
+//
+//  Created by Dreamoc on 2020/12/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ JLToolsFramework/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  JLToolsFramework
+//
+//  Created by Dreamoc on 2020/12/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
