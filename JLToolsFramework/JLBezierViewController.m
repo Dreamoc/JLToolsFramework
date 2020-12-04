75818bc7bfaa47a6cc9cc335120666de3fb997df
fengjinlin <461775260@qq.com>
2020-12-04 17:06:01 +0800

    添加Bezier

--- JLToolsFramework/JLBezierViewController.m
+++ JLToolsFramework/JLBezierViewController.m
@@ -22,76 +22,180 @@
 }
 
 - (void)create {
-    _view = [[MyBezierView alloc]initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 500)];
-    [self.view addSubview:_view];
-    _view.offsetY = 500;
+    [self drawNormal];
+    [self drawOvalInRect];
+    [self drawRoundedRect];
+    [self drawRoundedRectByRoundingCorners];
+    [self drawArcCenter];
+    [self drawLine];
+    [self drawCurveLine];
+    [self drawQuadCurveLine];
 
-    NSTimer * timer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(run) userInfo:nil repeats:YES];
-    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
-    
+    MyBezierView * view= [[MyBezierView alloc]initWithFrame:CGRectMake(100, 200, 200, 200)];
+    [self.view addSubview:view];
+//    NSTimer * timer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(run) userInfo:nil repeats:YES];
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
 
-- (void)run {
-    _view.offsetY-=1;
-    [_view setNeedsDisplay];
+- (void)drawNormal {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 50, 50)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 0.5;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawOvalInRect {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 50, 50)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 0.5;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawRoundedRect {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 50, 50) cornerRadius:10.f];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 2, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 0.5;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawRoundedRectByRoundingCorners {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 50, 50) byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft cornerRadii:CGSizeMake(20, 40)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 3, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 0.5;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawArcCenter {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(25, 25) radius:25 startAngle:M_PI_2 endAngle: 2 * M_PI clockwise:YES];
+//    或者
+//    UIBezierPath *bezierPath =  [UIBezierPath bezierPath];
+//    [bezierPath addArcWithCenter:CGPointMake(25, 25) radius:25 startAngle:M_PI_2 endAngle: 2 * M_PI clockwise:YES];
+    
+    bezierPath = [bezierPath bezierPathByReversingPath];//翻转
+    
+    [bezierPath appendPath:[bezierPath bezierPathByReversingPath]];//添加一个path
+        
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 4, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 0.5;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawLine {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+
+    [bezierPath moveToPoint:CGPointMake(0,0)];
+
+    [bezierPath addLineToPoint:CGPointMake(50, 50)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 5, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 1;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+- (void)drawCurveLine {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+
+    [bezierPath moveToPoint:CGPointMake(0,0)];
+
+    [bezierPath addCurveToPoint:CGPointMake(50, 50) controlPoint1:CGPointMake(0, 25) controlPoint2:CGPointMake(50, 25)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 6, 64 + 25);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 1;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
+}
+
+//二阶曲线
+- (void)drawQuadCurveLine {
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+
+    [bezierPath moveToPoint:CGPointMake(0,0)];
+    [bezierPath addQuadCurveToPoint:CGPointMake(50, 50) controlPoint:CGPointMake(0, 25)];
+    CAShapeLayer * shape = [CAShapeLayer new];
+    shape.frame = bezierPath.bounds;
+    shape.position = CGPointMake(25 + 50 * 0, 64 + 25 + 50);
+    shape.fillColor = [UIColor orangeColor].CGColor;
+    shape.lineWidth = 2;
+    shape.strokeColor = [UIColor redColor].CGColor;
+    shape.strokeStart = 0;
+    shape.strokeEnd = 1;
+    shape.path = bezierPath.CGPath;
+    [self.view.layer addSublayer:shape];
 }
 
 @end
 
 @implementation MyBezierView
-{
-    BOOL der;
-    UIBezierPath *path;
-}
-- (instancetype)init
-{
-    self = [super init];
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor orangeColor];
-        path = [UIBezierPath bezierPath];
-
     }
     return self;
 }
+
 - (void)drawRect:(CGRect)rect {
-    
-    // 创建一个贝塞尔曲线句柄
+    [super drawRect:rect];
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    [bezierPath moveToPoint:CGPointMake(0,0)];
 
+    [bezierPath addLineToPoint:CGPointMake(10, 50)];
 
-    // 初始化该path到一个初始点
-    path = [UIBezierPath bezierPath];
+    [bezierPath addLineToPoint:CGPointMake(100, 80)];
 
-    [path moveToPoint:CGPointMake(0, 0)];
+    bezierPath.lineWidth=5.0;
+//        [[UIColor redColor] setStroke];
+//        [[UIColor redColor] setFill];
+    [bezierPath fill];
 
-//    // 添加一条直线
-
-//    [path addLineToPoint:CGPointMake(0, 0)];
-
-    // 画二元曲线，一般和moveToPoint配合使用
-    NSLog(@"%.2f",_offsetY*1.2);
-    
-    [path addQuadCurveToPoint:CGPointMake(self.frame.size.width, 0) controlPoint:CGPointMake(self.frame.size.width/2, _offsetY*1.2)];
-
-    // 关闭该path
-
-    [path closePath];
-
-    // 创建描边（Quartz）上下文
-
-    CGContextRef context = UIGraphicsGetCurrentContext();
-
-    // 将此path添加到Quartz上下文中
-
-    CGContextAddPath(context, path.CGPath);
-
-    // 设置本身颜色
-
-    [[UIColor redColor] set];
-
-    // 设置填充的路径
-
-    CGContextFillPath(context);
+    [bezierPath stroke];
 
 }
-
 @end
